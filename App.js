80659c5b7496b0115a40b53555368001fba9d6f3
Nelson Millan <nelson.millan@globant.com>
2022-01-30 22:54:43 -0500

    Add new features about splash screen

--- App.js
+++ App.js
@@ -1,8 +1,22 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 
 // @screens
 import HomeScreen from './src/screens/HomeScreen';
 
-const App = () => <HomeScreen />;
+import SplashScreen from 'react-native-splash-screen';
+import {Platform, StatusBar} from 'react-native';
+
+const App = () => {
+  useEffect(() => {
+    Platform.OS !== 'ios' ? SplashScreen.hide() : null;
+  }, []);
+
+  return (
+    <>
+      <StatusBar backgroundColor="#5B4181" />
+      <HomeScreen />
+    </>
+  );
+};
 
 export default App;
